f53c0b630e3396b6335a5b42a5282e4f19972038
Utkarsh Shah <utkarshshah007@gmail.com>
2020-07-22 21:01:23 -0400

    fixes

--- Hexmodal Technologies_files/hex-js.js
+++ Hexmodal Technologies_files/hex-js.js
@@ -24,7 +24,7 @@ function nextPrev(n) {
   // This function will figure out which tab to display
   var x = document.getElementsByClassName("tab");
   // Exit the function if any field in the current tab is invalid:
-  //if (n == 1 && !validateForm()) return false;
+  if (n == 1 && !validateForm()) return false;
   // Hide the current tab:
   x[currentTab].style.display = "none";
   // Increase or decrease the current tab by 1:
@@ -37,6 +37,36 @@ function nextPrev(n) {
   }
   // Otherwise, display the correct tab:
   showTab(currentTab);
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false:
+      valid = false;
+    }
+  }
+  // If there are radio options...
+  if (x[currentTab].querySelectorAll('input[type="radio"]').length > 0) {
+    // If none of them are selected...
+    if (x[currentTab].querySelectorAll('input[type="radio"]:checked').length == 0) {
+      // set the current valid status to false:
+      valid = false;
+    }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; // return the valid status
 }
 
 function fixStepIndicator(n) {
