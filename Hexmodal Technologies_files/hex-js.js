1077cc116428ed24eddb9489851f8698c838e65b
Utkarsh Shah <utkarshshah007@gmail.com>
2020-08-17 18:59:18 -0400

    fix case study link

--- Hexmodal Technologies_files/hex-js.js
+++ Hexmodal Technologies_files/hex-js.js
@@ -10,28 +10,17 @@ $(function(){
     e.preventDefault();
     var modal = $('#exampleModal');
     modal.find('#exampleModalLabel').text('Sign Up For The Webinar!');
+    $('#popup-submit-button')[0].setAttribute( "onClick", "formSubmit('webinar');" );
     modal.modal('show');
   });
 })
 
 function formSubmit(type) {
-  // if it's a standard form... :
-  if (type == '#') {
-    //...the form gets submitted as usual:
-    document.getElementById("leadForm").submit();
-    //return false;
-  } else if (type == 'case-study') {
-    //...the form gets submitted by ajax:
-    $.ajax({
-      url: "https://formspree.io/sales@hexmodal.com",
-      method: "POST",
-      data: {full_name: $('#full_name').value, email: $('#email').value},
-      dataType: "json"
-    });
-    // and the user gets redirected to the requested file
-    document.location.href="https://portia.hexmodal.com/pilot-case-study.pdf"
-    return false;
+  if (type == 'case-study') {
+    // redirect the user gets redirected to the requested file
+    document.location.href="https://portia.hexmodal.com/pilot-case-study.pdf";
+  } else {
+    // Hide the modal
+    $('#exampleModal').modal('hide')
   }
-  // Otherwise, display the correct tab:
-  showTab(currentTab);
 }
